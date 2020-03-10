7a4ecb9e07a34208e5fa6245da0696a8755ba4b1
yaohao <37762826+isYaoHao@users.noreply.github.com>
2020-03-10 18:34:00 +0800

    Updates

---
+++ precache-manifest.a5355bac81103062c8a63bc490fe316c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d34d667d03335b5bff045b7925a6ee",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c5f2ed0755318e68137a",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d673010e73be17754ea",
+    "url": "/my-app/static/js/2.f9301b16.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.f9301b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f2ed0755318e68137a",
+    "url": "/my-app/static/js/main.de27320e.chunk.js"
+  },
+  {
+    "revision": "87b3c0b3450821036539",
+    "url": "/my-app/static/js/runtime-main.9136310f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
